53108cf27e9613afba21b0b57fc02bb42f47f61b
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-01-10 22:54:39 -0500

    Update users_controllers.rb

--- exerciseroutine/app/controllers/users_controllers.rb
+++ exerciseroutine/app/controllers/users_controllers.rb
@@ -1,98 +1,115 @@
-class UsersController < ApplicationController
-    # GET: /let the user go for the sign-up page --done
-    get "/signup" do
-      if signed_in?
-        redirect '/routines'
-      else
-        erb :"/users/new.html"
-      end
+class RoutinesController < ApplicationController
+  # GET: /routines asking the server for the data in routine -- done
+  get "/routines" do
+    
+    if signed_in?
+     
+      @user = User.find(session[:user_id])
+     
+
+        @routines = Routine.where(user_id: current_user)
+    
+        erb :"routines/index.html"
+    else
+      redirect "/signin"
     end
-    
-    post "/signup" do
+  end
+
  
-        if params[:name].empty? || params[:email].empty? || params[:password].empty?
-          redirect "/signup"
-        else
-          
-          @user = User.create(:name => params[:name], :email => params[:email], :password => params[:password])
-          # @user.save
-          session[:user_id] = @user.id
-          redirect "/routines"
-        end
-      end
+  get "/routines/new" do
+    if signed_in?
+      @user = User.find_by(id: session[:user_id])
+      erb :"/routines/new.html"
+    else
+      redirect "/signin"
+    end
+  end
 
-      post "/signin" do
-        @user = User.find_by(:name => params[:name])
-        if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect '/routines'
-        else
-          redirect "/signup"
-        end
-      end
 
-      get '/users' do
-        if signed_in?
-         
-          @user = User.find(session[:user_id])
-         
-            erb :"users/show.html"
-        else
-          redirect "/signin"
-        end
-      end
-
-      get "/signin" do
-        if signed_in?
-          redirect '/routines'
-        else
-          erb :"/users/signin.html"
-        end
-      end
-
-      get "/signout" do
+  post "/routines" do
+  
+    if signed_in?
+      @user = User.find(session[:user_id])
     
-        if signed_in?
-          session.destroy
-          redirect "/signin"
-        else
-          redirect "/index"
-        end
-      end
 
-      get "/users/:id/edit" do
-        @user = User.find_by(id: session[:user_id])
-        if @user
-    
+      if params[:exercise].empty?
+        redirect "/routines/new"
+      else
+      @user = User.find_by(:id => session[:user_id])
+      @routine = Routine.new
+      @routine.exercise = params[:exercise]
+      @routine.user_id = @user.id
+      @routine.save
+
         
-        erb :"/users/edit.html"
-        else
-          redirect "/signin"
-        end
+        redirect "/routines"
       end
+    else
+      redirect "/signin"
+    end
+  end
+  get '/routines/:id' do
+    if signed_in?
+      # @user = User.find_by(id: session[:user_id])
+      @routine = Routine.find(params[:id])
+      if @routine && @routine.user == current_user
+     
+      erb :'/routines/show.html'
+    else
+      redirect "/signin"
+    end
+    else
+      redirect '/signin'
+    end
+  end
 
-      patch '/users/:id' do
-      
-        if signed_in?
-          if params[:name].empty?
-            
-            redirect "/users/#{params[:id]}/edit"
+  get "/routines/:id/edit" do
+    @user = User.find_by(id: session[:user_id])
+    @routine = Routine.find(params[:id])
+    if @routine && @routine.user == current_user
+
+    
+    erb :"/routines/edit.html"
+    else
+      redirect "/routines"
+    end
+  end
+  patch '/routines/:id' do
+    if signed_in?
+      if params[:exercise].empty?
+        redirect "/routines/#{params[:id]}/edit"
+      else
+        @routine = Routine.find_by_id(params[:id])
+        if @routine && @routine.user == current_user
+          if @routine.update(:exercise => params[:exercise])
+            redirect to "/routines/#{@routine.id}"
           else
-            @user = User.find_by_id(params[:id])
-            if @user == current_user
-              if @user.update(:name => params[:name], :email => params[:email])
-                redirect to "/users/#{@user.id}"
-              else
-              redirect to "/users/#{@user.id}/edit"
-              end
-            else
-              redirect to '/users'
-            end
+          redirect to "/routines/#{@routine.id}/edit"
           end
         else
-          redirect '/signin'
+          redirect to '/routines'
         end
       end
+    else
+      redirect '/signin'
+    end
+  end
+
+  delete '/routines/:id/delete' do
+   if signed_in?
+     @user = User.find_by(id: session[:user_id]) if session[:user_id]
+     @routine = Routine.find_by_id(params[:id])
+     # binding.pry
+     if @routine && @routine.user == current_user
+       @routine.delete
+       redirect '/routines'
+     end
+   else
+     redirect to '/signin'
+   end
+ end
+end
+      
       
     
     
