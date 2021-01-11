401e7895681d56df93331c211f23d17e8be66a49
Kelvin Lanier <kelvinlanier.com>
2021-01-10 19:39:09 -0500

    added current user

--- exerciseroutine/app/controllers/application_controller.rb
+++ exerciseroutine/app/controllers/application_controller.rb
@@ -16,3 +16,9 @@ class ApplicationController < Sinatra::Base
     def signed_in?
       !!current_user
     end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
+end
