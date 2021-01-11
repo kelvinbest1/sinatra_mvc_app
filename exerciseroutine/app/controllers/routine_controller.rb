bc94434d4079c18340cba75d3ddd8ef1b1b6242c
Kelvin Lanier <kelvinlanier.com>
2021-01-10 20:42:57 -0500

    added post/routines

--- exerciseroutine/app/controllers/routine_controller.rb
+++ exerciseroutine/app/controllers/routine_controller.rb
@@ -21,3 +21,26 @@ get "/routines" do
       redirect "/signin"
     end
   end
+
+  post "/routines" do
+    
+    if signed_in?
+      @user = User.find(session[:user_id])
+    
+
+      if params[:exercise].empty?
+        redirect "/routines/new"
+      else
+      @user = User.find_by(:id => session[:user_id])
+      @routine = Routine.new
+      @routine.exercise = params[:exercise]
+      @routine.user_id = @user.id
+      @routine.save
+
+        
+        redirect "/routines"
+      end
+    else
+      redirect "/signin"
+    end
+  end
