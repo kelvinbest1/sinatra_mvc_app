358329b35edfd09d1109c685f2ed1d1f2be88a09
Kelvin Lanier <kelvinlanier.com>
2021-01-10 22:03:28 -0500

    added post signup

--- exerciseroutine/app/controllers/users_controllers.rb
+++ exerciseroutine/app/controllers/users_controllers.rb
@@ -8,3 +8,15 @@ class UsersController < ApplicationController
       end
     end
     
+    post "/signup" do
+ 
+        if params[:name].empty? || params[:email].empty? || params[:password].empty?
+          redirect "/signup"
+        else
+          
+          @user = User.create(:name => params[:name], :email => params[:email], :password => params[:password])
+          # @user.save
+          session[:user_id] = @user.id
+          redirect "/routines"
+        end
+      end
