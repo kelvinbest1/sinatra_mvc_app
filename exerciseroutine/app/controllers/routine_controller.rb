257ec77508f3a8f921fb359caba880777c71da43
Kelvin Lanier <kelvinlanier.com>
2021-01-10 19:47:26 -0500

    added /routines/new

--- exerciseroutine/app/controllers/routine_controller.rb
+++ exerciseroutine/app/controllers/routine_controller.rb
@@ -12,3 +12,12 @@ get "/routines" do
       redirect "/signin"
     end
   end
+
+  get "/routines/new" do
+    if signed_in?
+      @user = User.find_by(id: session[:user_id])
+      erb :"/routines/new.html"
+    else
+      redirect "/signin"
+    end
+  end
