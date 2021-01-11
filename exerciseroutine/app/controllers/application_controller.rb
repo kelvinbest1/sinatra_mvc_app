552957c728725bf092e60fd08e29a4003e48071d
Kelvin Lanier <kelvinlanier.com>
2021-01-10 19:36:07 -0500

    added controller info

--- exerciseroutine/app/controllers/application_controller.rb
+++ exerciseroutine/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -5,10 +6,6 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "Routine_secret"
   end
-
-  get "/" do
-    erb :welcome
-  end
-
-end
